f2b7af29547feb31e71ae1d28067af87c6f2c442
Dominik Courcelles <dominik.courcelles@gmail.com>
2017-11-29 12:17:01 -0500

    Amelioration du script pour l'evaluation du temps que prend l'execution des requetes en ajoutant un parametre pour l'adresse IP et en envoyant les fichers crees par wget dans /dev/null.

--- TP3/calculTempsMoyen.sh
+++ TP3/calculTempsMoyen.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-
-for i in {1..40}; do
-    ARRAY+=(132.207.12.210:8000)
-done
-
-time echo ${ARRAY[*]} | xargs -n 1 -P 40 wget -q
+if [[ -n $1 ]]
+then
+    for i in {1..40}; do
+        ARRAY+=($1)
+    done
+    time echo ${ARRAY[*]} | xargs -n 1 -P 40 wget -q -O /dev/null
+else
+    echo "Veuillez spécifier l'adresse IP du serveur."
+fi
